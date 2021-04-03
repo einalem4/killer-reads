86ce9c4761028ad211cd4ece8b71fcfa7ffaff6f
einalem4 <melanie@codehangar.io>
2021-04-03 10:19:32 -0600

    set-up fetch calls

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,10 +2,10 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (username && password) {
+  if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'post',
       body: JSON.stringify({
@@ -28,26 +28,27 @@ async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && password) {
+  if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'post',
       body: JSON.stringify({
         username,
+        email,
         password
       }),
       headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
   }
 }
 
-document.querySelector('').addEventListener('submit', signupFormHandler);
-
-document.querySelector('').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
