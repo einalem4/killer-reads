0e27b6819e6fbe6d6ce2eb348478e4e66235f31c
einalem4 <melanie@codehangar.io>
2021-04-08 19:40:16 -0600

    connect login/signup fetch calls, add comment, upvote, edit-post, delete-post to handlebars files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,21 +2,21 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#user-login').value.trim();
+  const password = document.querySelector('#user-password').value.trim();
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({
-        username,
+        email,
         password
       }),
       headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/discussions');
     } else {
       alert(response.statusText);
     }
