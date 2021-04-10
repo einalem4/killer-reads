21953a9c87070641a6724c86ff027b5891b4ee3f
Ian Jackson <ijacksondesign@gmail.com>
2021-04-10 10:52:16 -0400

    discussions can be filtered by genre

--- controllers/api/genre-routes.js
+++ controllers/api/genre-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     Genre.findOne({
         where: {
             id: req.params.id
--- controllers/discussions-routes.js
+++ controllers/discussions-routes.js
@@ -1,11 +1,5 @@
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
 const { Post, User, Comment, Genre } = require('../models');
-
-// discussions
-// router.get('/discussions', (req, res) => {
-//     res.render('discussions');
-// });
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -56,10 +50,56 @@ router.get('/', (req, res) => {
     });
 });
   
-// discussions genre
-router.get('/discussions/genre/:genreId', (req, res) => {
-// res.render('discussions');
-res.render('discussions', { genreId: req.params.genreId });
+router.get('/:id', (req, res) => {
+    Post.findAll({
+        where: {
+            genre_id: req.params.id
+        },
+        attributes: [
+            'id', 
+            'title', 
+            'author',
+            'post_text', 
+            'created_at'
+        ],
+        include: [
+            {
+                model: Genre,
+                attributes: ['name']
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id', 
+                    'comment_text', 
+                    'post_id', 
+                    'user_id', 
+                    'created_at'
+                ],
+                include: 
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('discussions', { 
+            posts, 
+            loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
