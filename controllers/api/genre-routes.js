661c9498fa0be97e64d0f4cb694b08d4783a931b
Ian Jackson <ijacksondesign@gmail.com>
2021-04-10 00:51:09 -0400

    started genre routes and created a genre seed

---
+++ controllers/api/genre-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post, Genre } = require('../../models');
+
+// get all genres
+router.get('/', (req, res) => {
+    Genre.findAll()
+    .then(dbGenreData => res.json(dbGenreData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    Genre.create({
+        name: req.body.name
+    })
+    .then(dbGenreData => res.json(dbGenreData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
