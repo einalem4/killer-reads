349cbd17954cdc6d86513ef059e75e30d2ec9493
Ian Jackson <ijacksondesign@gmail.com>
2021-04-12 00:01:55 -0400

    fixed login issue

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -101,6 +101,7 @@ router.post('/login', (req, res) => {
     }
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
+      req.session.email = dbUserData.email;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
