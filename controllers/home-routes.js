1f40998e702dcfe645e010ecb4664d8bfb47a688
Ian Jackson <ijacksondesign@gmail.com>
2021-04-12 00:29:30 -0400

    moved routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,4 +35,130 @@ router.get('/', (req, res) => {
     });
 });
 
+// login
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/discussions');
+    return;
+  }
+  res.render('login');
+});
+
+// request reset password
+router.get('/forgot-password', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('forgot-password');
+});
+
+// reset password
+router.get('/reset-password', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('reset-password');
+});
+
+//sign up
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/discussions');
+    return;
+  }
+  res.render('signup');
+});
+
+// edit post page
+router.get('/edit-post', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/edit-post');
+    return;
+  }
+  res.render('login');
+});
+
+// single post page
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_text', 'author', 'title', 'created_at'],
+    include: [{
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    },
+    {
+      model: Genre,
+      attributes: ['id', 'name']
+    }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+      // pass data to template
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// comments
+router.get('/posts-comments', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_text', 'title', 'created_at'],
+    include: [{
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+
+      res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
